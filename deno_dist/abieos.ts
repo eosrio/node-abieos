3dd192015c6528c03b105c2f98f65fb87dc957ec
galaxygur <igorberlenko7@gmail.com>
2023-05-04 06:32:11 +0800

    sixth. deno.cwd()

--- deno_dist/abieos.ts
+++ deno_dist/abieos.ts
@@ -1,5 +1,5 @@
 import { createRequire } from 'node:module';
-const require = createRequire(Deno.cwd());
+const require = createRequire(".");
 let modulePath = "../dist/abieos.node";
 let abieos = require(modulePath);
 export class Abieos {
